97fe1588578443bf390b9aea5a7da3e9e0e3903e
ANN <58825602+ANNA8880@users.noreply.github.com>
2019-12-13 17:01:43 +0300

    Add files via upload

---
+++ Dish/src/Dish/TestDish.java
@@ -0,0 +1,8 @@
+package Dish;
+
+public class TestDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Глина", "Синий", "Средний");
+        System.out.println(t);
+    }
+}
